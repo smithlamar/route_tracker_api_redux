0ea60dab8dc1fafbd11dcf970267ef896f6e6f43
Lamar J. Smith <smith.lamarj@gmail.com>
2018-01-13 16:49:21 -0600

    Finished basic implementation of core endpoints.

--- src/main/groovy/com/lamarjs/routetracker/controller/RouteTrackerApiController.groovy
+++ src/main/groovy/com/lamarjs/routetracker/controller/RouteTrackerApiController.groovy
@@ -1,9 +1,12 @@
 package com.lamarjs.routetracker.controller
 
+import com.lamarjs.routetracker.data.cta.api.common.Prediction
 import com.lamarjs.routetracker.data.cta.api.common.Route
 import com.lamarjs.routetracker.service.CtaApiRequestService
 import com.lamarjs.routetracker.service.CtaRouteAssembler
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -19,9 +22,19 @@ class RouteTrackerApiController {
         this.ctaApiRequestService = ctaApiRequestService
     }
 
-    @RequestMapping("/routes")
-    public List<Route> routes() {
+    @GetMapping("/routes")
+    public List<Route> getRoutes() {
         return ctaRouteAssembler.getAssembledRoutes().values().toList()
+    }
+
+    @GetMapping("/predictions/{routeId}")
+    public List<Prediction> getPredictions(
+            @PathVariable String routeId,
+            @RequestParam String stopId, @RequestParam(required = false) Integer resultsLimit) {
+        
+        return resultsLimit ? ctaApiRequestService.getPredictions(routeId, stopId, resultsLimit) : ctaApiRequestService
+                .getPredictions(routeId, stopId)
+
     }
 
 }
